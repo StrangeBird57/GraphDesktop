fd047e7a9aac9d1d84bbaa789b3103426ea97638
Vladislav Zhukov <102047649+StrangeBird57@users.noreply.github.com>
2024-03-25 14:20:26 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddVertexBtn = new System.Windows.Forms.Button();
+            this.RemoveVertexBtn = new System.Windows.Forms.Button();
+            this.AddEdgeBtn = new System.Windows.Forms.Button();
+            this.DfsBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddVertexBtn
+            // 
+            this.AddVertexBtn.Location = new System.Drawing.Point(12, 12);
+            this.AddVertexBtn.Name = "AddVertexBtn";
+            this.AddVertexBtn.Size = new System.Drawing.Size(150, 40);
+            this.AddVertexBtn.TabIndex = 1;
+            this.AddVertexBtn.Text = "Добавить вершину";
+            this.AddVertexBtn.UseVisualStyleBackColor = true;
+            this.AddVertexBtn.Click += new System.EventHandler(this.AddVertexBtn_Click);
+            // 
+            // RemoveVertexBtn
+            // 
+            this.RemoveVertexBtn.Location = new System.Drawing.Point(177, 12);
+            this.RemoveVertexBtn.Name = "RemoveVertexBtn";
+            this.RemoveVertexBtn.Size = new System.Drawing.Size(150, 40);
+            this.RemoveVertexBtn.TabIndex = 2;
+            this.RemoveVertexBtn.Text = "Удалить вершину";
+            this.RemoveVertexBtn.UseVisualStyleBackColor = true;
+            this.RemoveVertexBtn.Click += new System.EventHandler(this.RemoveVertexBtn_Click);
+            // 
+            // AddEdgeBtn
+            // 
+            this.AddEdgeBtn.Location = new System.Drawing.Point(345, 12);
+            this.AddEdgeBtn.Name = "AddEdgeBtn";
+            this.AddEdgeBtn.Size = new System.Drawing.Size(150, 40);
+            this.AddEdgeBtn.TabIndex = 3;
+            this.AddEdgeBtn.Text = "Добавить ребро";
+            this.AddEdgeBtn.UseVisualStyleBackColor = true;
+            this.AddEdgeBtn.Click += new System.EventHandler(this.AddEdgeBtn_Click);
+            // 
+            // DfsBtn
+            // 
+            this.DfsBtn.Location = new System.Drawing.Point(513, 12);
+            this.DfsBtn.Name = "DfsBtn";
+            this.DfsBtn.Size = new System.Drawing.Size(150, 40);
+            this.DfsBtn.TabIndex = 4;
+            this.DfsBtn.Text = "DFS";
+            this.DfsBtn.UseVisualStyleBackColor = true;
+            this.DfsBtn.Click += new System.EventHandler(this.DfsBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(832, 453);
+            this.Controls.Add(this.DfsBtn);
+            this.Controls.Add(this.AddEdgeBtn);
+            this.Controls.Add(this.RemoveVertexBtn);
+            this.Controls.Add(this.AddVertexBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AddVertexBtn;
+        private System.Windows.Forms.Button RemoveVertexBtn;
+        private System.Windows.Forms.Button AddEdgeBtn;
+        private System.Windows.Forms.Button DfsBtn;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        public class Edge
+        {
+            private int from, to;
+            private Pen color;
+
+            public Edge(int from, int to)
+            {
+                this.from = from;
+                this.to = to;
+                this.color = new Pen(Color.DarkBlue);
+            }
+
+            public void SetColor(Color new_color)
+            {
+                color = new Pen(new_color);
+            }
+
+            public Pen GetColor()
+            {
+                return color;
+            }
+
+            public int GetFrom()
+            {
+                return from;
+            }
+
+            public int GetTo()
+            {
+                return to;
+            }
+        }
+
+        List<Edge> Edges = new List<Edge>();
+        List<RoundButton> Vertexes = new List<RoundButton>();
+        List<List<int>> Graph = new List<List<int>>();
+        List<int> used = new List<int>();
+        List<Point> nums_pos = new List<Point>();
+        int free_id = 1;
+        int clicked_id = -1;
+        int first_id = -1;
+        int second_id = -1;
+        bool is_add_edge = false;
+        bool is_dfs = false;
+        bool clicked = false;
+        Point iOld = new Point();
+        Point iClick = new Point();
+        private Graphics graphics;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Вспомогательные функции
+
+        private Point ConvertFromChildToForm(int x, int y, Control control)
+        {
+            Point p = new Point(x, y);
+            control.Location = p;
+            return p;
+        }
+
+        private int GetIdByNum(int num)
+        {
+            int id = -1;
+            for (int i = 0; i < Vertexes.Count; ++i)
+            {
+                if (Vertexes[i].Text == num.ToString())
+                {
+                    id = i;
+                }
+            }
+            return id;
+        }
+
+        private int GetEdgeId(int from, int to)
+        {
+            for (int i = 0; i < Edges.Count; ++i)
+            {
+                int v = GetIdByNum(Edges[i].GetFrom());
+                int u = GetIdByNum(Edges[i].GetTo());
+                if ((u == from && v == to) || (u == to && v == from))
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+
+        private void DrawEdges()
+        {
+            graphics = CreateGraphics();
+            for (int i = 0; i < Edges.Count; ++i)
+            {
+                int first_vertex_id = GetIdByNum(Edges[i].GetFrom());
+                int second_vertex_id = GetIdByNum(Edges[i].GetTo());
+                Point first = new Point(Vertexes[first_vertex_id].Left + 25, Vertexes[first_vertex_id].Top + 25);
+                Point second = new Point(Vertexes[second_vertex_id].Left + 25, Vertexes[second_vertex_id].Top + 25);
+                graphics.DrawLine(Edges[i].GetColor(), first, second);
+            }
+        }
+
+        private void DrawNums()
+        {
+            for (int i = 0; i < nums_pos.Count; ++i)
+            {
+                graphics.DrawString((i + 1).ToString(), new Font("Arial", 12), new SolidBrush(Color.DarkBlue), nums_pos[i]);
+            }
+        }
+
+        public void ResetClicked(int cl = -1)
+        {
+            clicked_id = cl;
+            first_id = -1;
+            second_id = -1;
+            is_add_edge = false;
+            clicked = false;
+            is_dfs = false;
+        }
+
+        private void ToDeafult()
+        {
+            for (int i = 0; i < Vertexes.Count; ++i)
+            {
+                Vertexes[i].BackColor = Color.DarkBlue;
+            }
+            for (int i = 0; i < Edges.Count; ++i)
+            {
+                Edges[i].SetColor(Color.DarkBlue);
+            }
+            this.Refresh();
+            DrawEdges();
+        }
+
+        private void UpdateGraph()
+        {
+            Graph.Clear();
+            used.Clear();
+            int n = Vertexes.Count;
+            for (int i = 0; i < n; ++i)
+            {
+                List<int> tmp = new List<int>();
+                for (int j = 0; j < n; ++j)
+                {
+                    tmp.Add(0);
+                }
+                Graph.Add(tmp);
+                used.Add(0);
+            }
+            for (int i = 0; i < Edges.Count; ++i)
+            {
+                int first_vertex_id = GetIdByNum(Edges[i].GetFrom());
+                int second_vertex_id = GetIdByNum(Edges[i].GetTo());
+                Graph[first_vertex_id][second_vertex_id] = 1;
+                Graph[second_vertex_id][first_vertex_id] = 1;
+            }
+        }
+
+        private void dfs(int v)
+        {
+            used[v] = 1;
+            for (int u = 0; u < Graph[v].Count; ++u)
+            {
+                if (Graph[v][u] == 1 && used[u] != 1)
+                {
+
+                    Edges[GetEdgeId(v, u)].SetColor(Color.HotPink);
+                    DrawEdges();
+                    Thread.Sleep(500);
+                    Vertexes[u].BackColor = Color.HotPink;
+                    this.Refresh();
+                    int x = Vertexes[u].Left + 18;
+                    int y = Vertexes[u].Top - 15;
+                    nums_pos.Add(new Point(x, y));
+                    DrawNums();
+                    DrawEdges();
+                    Thread.Sleep(500);
+                    dfs(u);
+                }
+            }
+        }
+
+        // Обработка кликов мышью
+
+        private void Form_MouseClick(object sender, MouseEventArgs e)
+        {
+            ToDeafult();
+            if (clicked_id != -1)
+            {
+                Vertexes[clicked_id].BackColor = Color.DarkBlue;
+                clicked_id = -1;
+            }
+            if (is_add_edge)
+            {
+                if (first_id != -1)
+                {
+                    Vertexes[GetIdByNum(first_id)].BackColor = Color.DarkBlue;
+                }
+                if (second_id != -1)
+                {
+                    Vertexes[GetIdByNum(second_id)].BackColor = Color.DarkBlue;
+                }
+                is_add_edge = false;
+            }
+        }
+
+        private void Vertex_MouseClick(object sender, EventArgs e)
+        {
+            if (is_dfs)
+            {
+                ResetClicked();
+                ToDeafult();
+                int dfs_id = -1;
+                nums_pos.Clear();
+                for (int i = 0; i < Vertexes.Count(); ++i)
+                {
+                    if (sender.Equals(Vertexes[i]))
+                    {
+                        dfs_id = Convert.ToInt32(Vertexes[i].Text);
+                        Vertexes[i].BackColor = Color.HotPink;
+                    }
+                }
+                this.Refresh();
+                int x = Vertexes[GetIdByNum(dfs_id)].Left + 18;
+                int y = Vertexes[GetIdByNum(dfs_id)].Top - 15;
+                nums_pos.Add(new Point(x, y));
+                DrawNums();
+                DrawEdges();
+                UpdateGraph();
+                Thread.Sleep(500);
+                dfs(GetIdByNum(dfs_id));
+                is_dfs = false;
+            }
+            if (is_add_edge)
+            {
+                if (first_id == -1)
+                {
+                    for (int i = 0; i < Vertexes.Count(); ++i)
+                    {
+                        if (sender.Equals(Vertexes[i]))
+                        {
+                            first_id = Convert.ToInt32(Vertexes[i].Text);
+                            Vertexes[i].BackColor = Color.HotPink;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < Vertexes.Count(); ++i)
+                    {
+                        if (sender.Equals(Vertexes[i]))
+                        {
+                            second_id = Convert.ToInt32(Vertexes[i].Text);
+                            Vertexes[i].BackColor = Color.HotPink;
+                        }
+                    }
+                    int first_vertex_id = GetIdByNum(first_id);
+                    int second_vertex_id = GetIdByNum(second_id);
+                    Point first = new Point(Vertexes[first_vertex_id].Left + 25, Vertexes[first_vertex_id].Top + 25);
+                    Point second = new Point(Vertexes[second_vertex_id].Left + 25, Vertexes[second_vertex_id].Top + 25);
+                    graphics.DrawLine(Pens.DarkBlue, first, second);
+                    Vertexes[first_vertex_id].BackColor = Color.DarkBlue;
+                    Vertexes[second_vertex_id].BackColor = Color.DarkBlue;
+                    Edges.Add(new Edge(first_id, second_id));
+                    is_add_edge = false;
+                    first_id = -1;
+                    second_id = -1;
+                }
+            }
+        }
+
+        private void Vertex_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!is_add_edge && !is_dfs)
+            {
+                ResetClicked();
+                ToDeafult();
+                if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                {
+                    for (int i = 0; i < Vertexes.Count(); ++i)
+                    {
+                        if (sender.Equals(Vertexes[i]))
+                        {
+                            clicked_id = i;
+                            Vertexes[clicked_id].BackColor = Color.HotPink;
+                        }
+                    }
+                    Point p = ConvertFromChildToForm(e.X, e.Y, Vertexes[clicked_id]);
+                    iOld.X = p.X;
+                    iOld.Y = p.Y;
+                    iClick.X = e.X;
+                    iClick.Y = e.Y;
+                    clicked = true;
+                }
+            }
+
+        }
+
+        private void Vertex_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (clicked)
+            {
+                Point p = new Point();
+                p.X = e.X + Vertexes[clicked_id].Left;
+                p.Y = e.Y + Vertexes[clicked_id].Top;
+                /*if (p.X <= this.Width && p.Y <= this.Height)
+                {
+                    Vertexes[clicked_id].Left = p.X - iClick.X;
+                    Vertexes[clicked_id].Top = p.Y - iClick.Y;
+                    graphics.Clear(Color.White);
+                    DrawEdges();
+                }*/
+                Vertexes[clicked_id].Left = p.X - iClick.X;
+                Vertexes[clicked_id].Top = p.Y - iClick.Y;
+                graphics.Clear(Color.White);
+                DrawEdges();
+            }
+
+        }
+
+        private void Vertex_MouseUp(object sender, MouseEventArgs e)
+        {
+            clicked = false;
+        }
+
+        private void AddVertexBtn_Click(object sender, EventArgs e)
+        {
+            ResetClicked();
+            ToDeafult();
+            RoundButton tmp = new RoundButton();
+            tmp.Text = free_id.ToString();
+            tmp.Width = 50;
+            tmp.Height = 50;
+            tmp.BackColor = Color.DarkBlue;
+            tmp.ForeColor = Color.White;
+            tmp.MouseDown += Vertex_MouseDown;
+            tmp.MouseUp += Vertex_MouseUp;
+            tmp.MouseMove += Vertex_MouseMove;
+            tmp.MouseClick += Vertex_MouseClick;
+            tmp.Location = new Point(100, 100);
+            this.Controls.Add(tmp);
+            Vertexes.Add(tmp);
+            ++free_id;
+            DrawEdges();
+        }
+
+        private void RemoveVertexBtn_Click(object sender, EventArgs e)
+        {
+            ResetClicked(clicked_id);
+            ToDeafult();
+            if (clicked_id != -1)
+            {
+                List<Edge> remove_edges = new List<Edge>();
+                for (int i = 0; i < Edges.Count; ++i)
+                {
+                    int first = GetIdByNum(Edges[i].GetFrom());
+                    int second = GetIdByNum(Edges[i].GetTo());
+                    if (first == clicked_id || second == clicked_id)
+                    {
+                        remove_edges.Add(Edges[i]);
+                    }
+                }
+                for (int i = 0; i < remove_edges.Count; ++i)
+                {
+                    Edges.Remove(remove_edges[i]);
+                }
+
+                this.Controls.Remove(Vertexes[clicked_id]);
+                Vertexes.Remove(Vertexes[clicked_id]);
+                ResetClicked();
+                ToDeafult();
+            }
+        }
+
+        private void AddEdgeBtn_Click(object sender, EventArgs e)
+        {
+            ResetClicked();
+            ToDeafult();
+            is_add_edge = true;
+        }
+
+        private void DfsBtn_Click(object sender, EventArgs e)
+        {
+            is_dfs = true;
+            if (clicked_id != -1)
+            {
+                Vertexes[clicked_id].BackColor = Color.DarkBlue;
+                clicked_id = -1;
+            }
+        }
+    }
+
+    public class RoundButton : Button
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath grPath = new GraphicsPath();
+            grPath.AddEllipse(2, 2, ClientSize.Width - 5, ClientSize.Height - 5);
+            this.Region = new System.Drawing.Region(grPath);
+            base.OnPaint(e);
+        }
+    }
+}
